1ad77e9ea97bf1923fccc97ddac28993ada187d5
Vayid Moussa <vayidmoussa@tfl.gov.uk>
2022-03-05 14:29:37 +0000

    Formatted with black

--- main.py
+++ main.py
@@ -4,19 +4,25 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-@app.get('/')
-async def root():
-    return {'message': 
-    'This is a simple API for my coding exercise. Please visit /docs or /redoc for more information on how to use it.'}
 
-@app.get('/hello')
+@app.get("/")
+async def root():
+    return {
+        "message": "This is a simple API for my coding exercise. "
+        "Please visit /docs or /redoc for more information on how to use it."
+    }
+
+
+@app.get("/hello")
 async def hello():
-    return {'message': 'Hello World!'}
+    return {"message": "Hello World!"}
+
 
 @app.get("/add")
 async def add(num1: int, num2: int):
-    return {'result': num1 + num2}
+    return {"result": num1 + num2}
+
 
 @app.get("/addwords")
 async def addwords(word1: str, word2: str):
-    return {'result': f"{word1}-{word2}"}
+    return {"result": f"{word1}-{word2}"}
