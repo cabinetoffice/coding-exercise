e7165e2fe52e0aca6240c40feaada7523ff83734
Vayid Moussa <vayidmoussa@tfl.gov.uk>
2022-03-05 15:16:47 +0000

    Updated deprecated calls

--- tests/unit/test_main.py
+++ tests/unit/test_main.py
@@ -9,31 +9,31 @@ client = TestClient(app)
 class TestAPI(unittest.TestCase):
     def test_hello(self):
         response = client.get("/hello")
-        self.assertEquals(response.status_code, 200)
-        self.assertEquals(response.json(), {"message": "Hello World!"})
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.json(), {"message": "Hello World!"})
 
     def test_add(self):
         """Testing the addition of two numbers."""
 
         response = client.get("/add", params={"num1": 5, "num2": 10})
-        self.assertEquals(response.status_code, 200)
-        self.assertEquals(response.json(), {"result": 15})
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.json(), {"result": 15})
 
     def test_add_with_string(self):
         """Tests to see if a string provided returns a failure message non 200 HTTP
         status code, indicating incorrect type provided."""
 
         response = client.get("/add", params={"num1": "Not a number", "num2": "10"})
-        self.assertEquals(response.status_code, 422)
-        self.assertEquals(response.json()["detail"][0]["msg"],
+        self.assertEqual(response.status_code, 422)
+        self.assertEqual(response.json()["detail"][0]["msg"],
                           "value is not a valid integer")
 
     def test_join_words(self):
         """Testing the joining of two words with a dash in between."""
 
         response = client.get("/join-words", params={"word1": "hello", "word2": "world"})
-        self.assertEquals(response.status_code, 200)
-        self.assertEquals(response.json(), {"result": "hello-world"})
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.json(), {"result": "hello-world"})
 
 
 if __name__ == "__main__":
