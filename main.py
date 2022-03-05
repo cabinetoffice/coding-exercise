02a212980597c40324f0eaf13278ba24c776bf63
Vayid <>
2022-03-05 17:10:34 +0000

    Tidy up names for redoc

--- main.py
+++ main.py
@@ -20,12 +20,12 @@ def hello():
 
 
 @app.get("/add")
-def add(num1: int, num2: int):
+def add_numbers(num1: int, num2: int):
     """Takes two numbers and returns the result of the addition."""
     return {"result": num1 + num2}
 
 
 @app.get("/join-words")
-def addwords(word1: str, word2: str):
+def join_words(word1: str, word2: str):
     """Takes two words and returns a joined word separated by a dash."""
     return {"result": f"{word1}-{word2}"}
